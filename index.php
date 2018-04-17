87394e309805bf1646288e8680ee82472af0edc8
yuriningtyas <tiiyas.akakom42@gmail.com>
2018-04-17 15:16:26 +0700

    nambah index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+<html>
+<body> 
+	echo "Selamat datang di Github Tiiyas";
+	echo "<br/>";
+	echo "Praktikum Teknologi Cloud Computing";
+	echo "<br/>";
+	echo "Yuriningtyas Istiqomah";
+	echo "<br/>";
+	echo "155610042";
+</body>
+</html>
+?>
